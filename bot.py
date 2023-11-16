c5164692e8d772a07e67d045b879ec1a5c75f237
Oleh Kinash <o.qinash@gmail.com>
2023-11-16 22:33:39 +0200

    push

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 from googletrans import Translator
 from json_translator import translateJson
 from xml_translator import translateXml
+import zipfile
 import sys
 load_dotenv()
 
@@ -70,9 +71,9 @@ def handle_language_selection(message):
         bot.register_next_step_handler(message, handle_custom_language)
         return
 
-    if selected_language not in language_codes:
-        bot.send_message(chat_id, "Будь ласка, вибери коректну мову зі списку.")
-        return
+    # if selected_language not in language_codes:
+    #     bot.send_message(chat_id, "Будь ласка, вибери коректну мову зі списку.")
+    #     return
 
     bot.send_message(chat_id, "Тепер дай мені файл для перекладу.")
     bot.register_next_step_handler(message, handle_translation, selected_language)
@@ -112,16 +113,33 @@ def handle_translation(message, selected_language):
                     handle_start(message)
         elif 'xml' in file_extension:
             bot.send_message(message.chat.id, f"Перекладаю на: {selected_language}.")
+            folder_names = []
             for language_name in languages_to_translate:
                 lang = language_name.strip()
                 file_url = f"https://api.telegram.org/file/bot{telegram_token}/{file_info.file_path}"
                 target_language = language_codes.get(lang, lang)
                 # Download the file
-                downloaded_file_path = f"{file_name}_{target_language}.{file_extension}"
+                os.makedirs(f"values-{target_language}", exist_ok=True)
+                downloaded_file_path = f"values-{target_language}/{file_name}.{file_extension}"
                 os.system(f"curl -o {downloaded_file_path} {file_url}")
                 translateXml(downloaded_file_path, target_language)
 
+                folder_names.append(f"values-{target_language}")
+            zip_filename = 'archive.zip'
+            with zipfile.ZipFile(zip_filename, 'w') as zip_file:
+                for folder_name in folder_names:
+                    for root, dirs, files in os.walk(folder_name):
+                        for file in files:
+                            file_path = os.path.join(root, file)
+                            arcname = os.path.relpath(file_path, folder_name)
+                            zip_file.write(file_path, os.path.join(folder_name, arcname))
+
+            with open(zip_filename, 'rb') as zip_file:
+                bot.send_document(message.chat.id, zip_file)
+
+            os.remove(zip_filename)
+
 
 if __name__ == '__main__':
-    bot.polling()
+    bot.infinity_polling(none_stop=True)
 
